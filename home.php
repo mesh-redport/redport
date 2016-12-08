1fc4149b8a8957c77a6c10aca9c00ca614891a38
Herbert Spencer <herbert.spencer@gmail.com>
2016-12-08 18:44:40 -0300

    menu es un parcial ahora

--- home.php
+++ home.php
@@ -19,18 +19,7 @@
       <div class="menu-button"><a href="#"><div class="rpicon-menu"></div></a></div>
     </div>
 
-    <!--Inicio menú lateral-->
-    <nav class="menu_nav">
-      <ul>
-        <li><a href="home.html">Inicio</a></li>
-        <li><a href="reportar.html">reportar</a></li>
-        <li><a href="home.html#mi_ficha">mi ficha</a></li>
-        <li><a href="estado_hogar.html">hogar</a></li>
-        <li><a href="reportar_entorno.html">entorno</a></li>
-        <li><a href="home.html#reportes">últimos reportes</a></li>
-      </ul>
-    </nav>
-    <!--Fin menú lateral-->
+    <?php include "menu.php" ?>
 
     <!-- ======= Fin header======= -->
     <?php
---
+++ menu.php
@@ -0,0 +1,12 @@
+<!--Inicio menú lateral-->
+    <nav class="menu_nav">
+      <ul>
+        <li><a href="home.php?rut=<?php echo $_REQUEST['rut'] ?>" >Inicio</a></li>
+        <li><a href="reportar.html">reportar</a></li>
+        <li><a href="home.html#mi_ficha">mi ficha</a></li>
+        <li><a href="estado_hogar.html">hogar</a></li>
+        <li><a href="reportar_entorno.html">entorno</a></li>
+        <li><a href="home.html#reportes">últimos reportes</a></li>
+      </ul>
+    </nav>
+<!--Fin menú lateral-->
